d5d038e2316ba7eff54364863448e6fbab1e5010
cyiano <cyiano@outlook.com>
2026-03-11 13:11:24 +0800

    Initial Commit

---
+++ ClimbingTracker/ClimbingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbingTrackerApp.swift
+//  ClimbingTracker
+//
+//  Created by 江惠娟 on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct ClimbingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimbingTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClimbingTracker
+//
+//  Created by 江惠娟 on 2026/3/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
